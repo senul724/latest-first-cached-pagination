2ffbde41f6dd4db65d2b3a23eb6129c406323cd4
senul ranasinghe <hardlove.mrplus@gmail.com>
2024-08-22 13:08:54 +0530

    index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Pagination Master
         </h1>
+        <p>
+          Efficient and cool way to use pagination for data organised from
+          latest to old with caching
+        </p>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            href="view/1"
           >
-            <h3 className="text-2xl font-bold">View demo</h3>
+            <h3 className="text-2xl font-bold border shadow-xl rounded py-2 px-4 hover:scale-105">View demo</h3>
           </Link>
         </div>
       </div>
